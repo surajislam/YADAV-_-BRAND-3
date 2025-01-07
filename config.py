f7d52eddcddb93add8484d3e4ed4a8952b8ebea6
surajislam <154444093+surajislam@users.noreply.github.com>
2025-01-07 20:36:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/surajislam/YADAV_BRAND",
+    "https://github.com/surajislam/YADAV-_-BRAND-3",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
